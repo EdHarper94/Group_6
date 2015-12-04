1235bf980bfeeee96aea73d4b23a5428e1ab79d8
jomichael94 <jomichael1994@gmail.com>
2015-12-04 00:21:33 +0000

    Created initial GameResult class
    Currently being worked on

---
+++ GameResult.java
@@ -0,0 +1,11 @@
+/**
+ * @file GameResult.java
+ * @author James Michael
+ * @date 03 Dec 2015
+ *
+ * This class will be used to help determine the result of the game
+ */
+
+public class GameResult{
+	
+}

7b2981142da81f96e324e304088fd0959105b006
jomichael94 <jomichael1994@gmail.com>
2015-12-04 00:33:08 +0000

    Created initial GameResult class, added brief comments

--- GameResult.java
+++ GameResult.java
@@ -7,5 +7,22 @@
  */
 
 public class GameResult{
+	private boolean gameWon;
+	private boolean gameLost;
+
+	//if the game result is being handled in another class, pass to this:
+	/*public GameResult(boolean result){
+		setGameResult(result);
+	}*/
+
+	//this method will handle whether won/lost, possibly animation (i'm thinking JOptionPane)
+	public void setGameResult(gameWon, gameLost){
+
+	}
+
+	//this method will return the appropriate outcome
+	public boolean getGameResult{
+
+	}
 	
 }

b0dcd9b08b9284c34a34f0b9a6b5c965061c9fd7
ifanviv <ifan16@live.co.uk>
2015-12-07 03:31:39 +0000

    j

--- sunday2/Counter.java
+++ sunday2/Counter.java
@@ -1,10 +1,26 @@
-
+/**
+ * @file Counter.java
+ * @author Xiao
+ * @date 04/12/2015
+ * @see Board.java
+ * @see Diffuse.java
+ * @see Mine.java
+ * @see RevealAlgorithm.ava
+ * @see Tile.java
+ */
 public class Counter {
 	
 	static int m_MineCount;
 	static int m_DiffusedCount;
 	static int m_RevealedTileCount;
 	static int m_TilesRemainingCount;
+
+	/**
+	 * A normal member taking two arguments and returning
+	 * @param numOfMines an integer argument.
+	 * @param numOfTiles an integer argument.
+	 * @return a constructor returns an instance of the object.
+	 */
 	
 	public Counter(int numOfMines, int numOfTiles){
 		m_DiffusedCount = 0;
@@ -12,39 +28,85 @@ public class Counter {
 		m_MineCount = numOfMines;
 		m_TilesRemainingCount = numOfTiles*numOfTiles;
 	}
+	/**
+	 * A normal member taking one argument and returning.
+	 * @param numOfMines an integer argument.
+	 * @return the number of mines.
+	 */
 	
 	public static void setMineCount(int numOfMines){
 		m_MineCount = numOfMines;
 	}
-	
+
+	/**
+	 * A normal member taking one argument and returning
+	 * @param numOfTiles an integer argument.
+	 * @return reducing the number of tiles remaining.
+	 */
+
 	public static void decrementTilesRemainingCount(){
 		m_TilesRemainingCount --;
 	}
 
+	/**
+	 * takes no arguments
+	 * @return the number of diffused tiles gets lower
+	 */
+
 	public static void decrementDiffusedCount(){
 		m_DiffusedCount --;
 	}
+
+	/**
+	 * takes no arguments
+	 * @return the counter of diffused tiles gets higher
+	 */
 	
 	public static void incrementDiffusedCount(){
 		m_DiffusedCount ++;
 	}
-	
+
+	/**
+	 * takes no arguments
+	 * @return increments the number of revealed tile counter
+	 */
+
 	public static void incrementRevealedTileCount(){
 		m_RevealedTileCount ++;
 	}
-	
+
+	/**
+	 * takes no arguments
+	 * @return the number of mines in on the board
+	 */
+
 	public static int getMineCount(){
 		return m_MineCount;
 	}
 
+	/**
+	 * takes no arguments
+	 * @return the number of tiles remaining
+	 */
+
 	public static int getTilesRemainingCount(){
 		return m_TilesRemainingCount;
 	}
-	
+
+	/**
+	 * takes no arguments
+	 * @return the number of revealed tiles
+	 */
+
 	public static int getRevealedTileCount(){
 		return m_RevealedTileCount;
 	}
-	
+
+	/**
+	 * takes no arguments
+	 * @return the number of diffused tiles
+	 */
+
 	public static int getDiffusedCount(){
 		return m_DiffusedCount;
 	}
